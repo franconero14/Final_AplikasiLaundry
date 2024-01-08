10d45e54d70608c31d67ab66730d1eb9e6632f2a
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-08 21:11:56 +0700

    Membuat object DataPewangi

---
+++ app/src/main/java/com/example/aplikasilaundry/data/DataPewangi.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasilaundry.data
+
+import com.example.lebe.R
+
+object DataPewangi {
+    val paket = listOf(
+        R.string.mawar,
+        R.string.oxib,
+        R.string.indofresh,
+        R.string.snap
+    )
+}
