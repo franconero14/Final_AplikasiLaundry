247adadd1b05982a2e40ff154cc603ec424c089e
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-08 21:48:40 +0700

    Membuat class PesananRepository dan menambahkan fun getAllPesananStream

---
+++ app/src/main/java/com/example/aplikasilaundry/repositori/PesananRepository.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasilaundry.repositori
+
+import com.example.aplikasilaundry.data.Pesanan
+import kotlinx.coroutines.flow.Flow
+
+interface PesananRepository {
+    fun getAllPesananStream(): Flow<List<Pesanan>>
+
+}
