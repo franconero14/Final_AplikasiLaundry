f9a99395e1043fc4970b9ce3bc7ef6748df181d4
franconero14 <nerobalor@gmail.com>
2024-01-09 01:19:09 +0700

    create data class DetailLaundry in FormViewModel

---
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/FormViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasilaundry.ui.model
+
+
+data class DetailLaundry(
+    val id: Int = 0,
+    val nama: String = "",
+    val nohp: String = "",
+    val alamat: String = "",
+    val harga: String = "",
+    val wangi: String = ""
+)
