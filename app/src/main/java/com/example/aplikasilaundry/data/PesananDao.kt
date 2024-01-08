9f15c03263f0662a8ef786137b20ac6fd4484d53
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-08 20:57:57 +0700

    Menambahkan Update di interface PesananDao

--- app/src/main/java/com/example/aplikasilaundry/data/PesananDao.kt
+++ app/src/main/java/com/example/aplikasilaundry/data/PesananDao.kt
@@ -3,9 +3,13 @@ package com.example.aplikasilaundry.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface PesananDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(pesanan: Pesanan)
+
+    @Update
+    suspend fun update(pesanan: Pesanan)
 }
