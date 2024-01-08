1b2e340b2fba1ad6cf83518fcdd4274536a28e7e
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-08 20:51:57 +0700

    Menambahkan data class pesanan

--- app/src/main/java/com/example/aplikasilaundry/data/Pesanan.kt
+++ app/src/main/java/com/example/aplikasilaundry/data/Pesanan.kt
@@ -1,4 +1,15 @@
 package com.example.aplikasilaundry.data
 
-class Pesanan {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_pesanan")
+data class Pesanan(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String = "",
+    val nohp: String = "",
+    val alamat: String = "",
+    val harga: String = "",
+    val wangi: String = ""
+)
