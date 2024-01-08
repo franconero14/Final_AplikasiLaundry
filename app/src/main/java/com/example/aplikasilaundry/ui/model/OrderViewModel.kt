942a71766f6a6a257e2bdb3b38e5328df111b923
franconero14 <nerobalor@gmail.com>
2024-01-09 01:53:22 +0700

    Add data class OrderUiState in OrderViewModel

--- app/src/main/java/com/example/aplikasilaundry/ui/model/OrderViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/OrderViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.aplikasilaundry.ui.model
 
 import androidx.lifecycle.ViewModel
+import com.example.aplikasilaundry.data.Pesanan
 import com.example.aplikasilaundry.repositori.PesananRepository
 
 class OrderViewModel(private val pesananRepository: PesananRepository) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class OrderUiState(
+        val listPenyewa: List<Pesanan> = listOf()
+    )
 }
