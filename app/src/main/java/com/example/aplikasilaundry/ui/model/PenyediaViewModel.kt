edb1e79a60da437000c502b63930649a57df11be
franconero14 <nerobalor@gmail.com>
2024-01-09 01:58:07 +0700

    Create object PenyediaViewModel

--- app/src/main/java/com/example/aplikasilaundry/ui/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/PenyediaViewModel.kt
@@ -4,5 +4,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.aplikasilaundry.AplikasiLaundry
 
+object PenyediaViewModel {
+}
+
 fun CreationExtras.aplikasiLaundry(): AplikasiLaundry =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiLaundry)
