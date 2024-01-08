ca2d9a9b7ef147ce5f80b5392f6a3d73aff9fdc5
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-08 21:07:09 +0700

    Menambahkan fun getPesanan dan fun getAllpesanan di interface PesananDao

--- app/src/main/java/com/example/aplikasilaundry/data/PesananDao.kt
+++ app/src/main/java/com/example/aplikasilaundry/data/PesananDao.kt
@@ -4,7 +4,9 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PesananDao {
@@ -16,4 +18,10 @@ interface PesananDao {
 
     @Delete
     suspend fun  delete(pesanan: Pesanan)
+
+    @Query("SELECT * from tbl_pesanan WHERE id = :id")
+    fun getPesanan(id: Int): Flow<Pesanan>
+
+    @Query("SELECT * from tbl_pesanan ORDER BY nama ASC")
+    fun getAllPesanan(): Flow<List<Pesanan>>
 }
