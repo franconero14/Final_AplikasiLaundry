c851602b3504a849564dce0a5c51d2535f18c8f7
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-10 03:34:12 +0700

    Membuat fun Listpesanan di OrderPage

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/OrderPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/OrderPage.kt
@@ -1,11 +1,14 @@
 package com.example.aplikasilaundry.ui.pages
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -15,6 +18,28 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.data.Pesanan
+
+@Composable
+fun Listpesanan(
+    listLaundry: List<Pesanan>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Pesanan) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(
+            items = listLaundry,
+            key = {
+                it.id
+            }
+        ) { customer ->
+            DataLaundry(
+                pesanan = customer,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { onItemClick(customer) })
+        }
+    }
+}
 
 @Composable
 fun DataLaundry(
