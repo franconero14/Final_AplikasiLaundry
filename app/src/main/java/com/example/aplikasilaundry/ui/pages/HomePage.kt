5399a4e6f1efa68f6ba193b6bb8e2effc1decd1e
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-09 22:41:15 +0700

    Menambahkan fun HomePage

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/HomePage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/HomePage.kt
@@ -1,9 +1,45 @@
 package com.example.aplikasilaundry.ui.pages
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.navigasi.DestinasiHalaman
 
 object HomeDestinasi : DestinasiHalaman {
     override val route = "laundry"
     override val judulHal = R.string.app_name
+}
+
+@Composable
+fun Homepage(
+    onNextButtonClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            border = BorderStroke(1.dp, Color.Cyan),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(50.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+
+        }
+    }
 }
