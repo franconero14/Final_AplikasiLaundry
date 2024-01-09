f2b478405f22b62e144d54cb3bed5c46ee4e6d6f
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-10 03:30:30 +0700

    menambahkan column dan outlinetextfield di FormInputPesanan

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/FormPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/FormPage.kt
@@ -2,22 +2,30 @@ package com.example.aplikasilaundry.ui.pages
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplikasilaundry.R
@@ -115,4 +123,68 @@ fun FormInputPesanan(
     onSelectionChanged: (String) -> Unit
 
 ) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(modifier = modifier.padding(top = 24.dp))
+        OutlinedTextField(
+            value = detailLaundry.nama,
+            onValueChange = { onValueChange(detailLaundry.copy(nama = it)) },
+            label = { Text(text = stringResource(id = R.string.namaUser)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailLaundry.alamat,
+            onValueChange = { onValueChange(detailLaundry.copy(alamat = it)) },
+            label = { Text(text = stringResource(id = R.string.alamatUser)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailLaundry.nohp,
+            onValueChange = { onValueChange(detailLaundry.copy(nohp = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = stringResource(id = R.string.hpUser)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        Column(
+            modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.pewangi))
+            pilihanSewa.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = detailLaundry.wangi == item,
+                    onClick = {
+                        onValueChange(detailLaundry.copy(wangi = item))
+                        onSelectionChanged(item)
+                    }
+                ),
+                    verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = detailLaundry.wangi == item,
+                        onClick = {
+                            onValueChange(detailLaundry.copy(wangi = item))
+                            onSelectionChanged(item)
+                        }
+                    )
+                    Text(item)
+                }
+            }
+        }
+        OutlinedTextField(
+            value = detailLaundry.harga,
+            onValueChange = { onValueChange(detailLaundry.copy(harga = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = stringResource(id = R.string.hargaUser)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
 }
