1c7bafe54f363ba5c0a60971d0de232d33e1c1f3
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-10 03:39:19 +0700

    Membuat object dan menambahkan fun PesananScreen OrderPage

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/OrderPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/OrderPage.kt
@@ -9,17 +9,79 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.data.Pesanan
+import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+import com.example.aplikasilaundry.navigasi.LaundryAppBar
+import com.example.aplikasilaundry.ui.model.OrderViewModel
+import com.example.aplikasilaundry.ui.model.PenyediaViewModel
+
+object OrderDestinasi : DestinasiHalaman {
+    override val route = "home"
+    override val judulHal = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PesananScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    onNavigateBack: () -> Unit,
+    viewModel: OrderViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            LaundryAppBar(
+                title = stringResource(id = OrderDestinasi.judulHal),
+                bisaNavigasiBack = false,
+                navigasiUp = { onNavigateBack })
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry, shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.btn_add)
+                )
+            }
+        }) { innerPadding ->
+        val uiStatePesanan by viewModel.homeUiState.collectAsState()
+        HomeBody(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
+            dataLaundry = uiStatePesanan.listPenyewa,
+            onPesananClick = onDetailClick
+        )
+    }
+}
+
 
 @Composable
 fun HomeBody(
