837f0ae8d5c16c3da48a282d7bafcf6ce30f4b8d
franconero14 <nerobalor@gmail.com>
2024-01-09 15:48:36 +0700

    add ResultViewModel

--- app/src/main/java/com/example/aplikasilaundry/ui/model/ResultViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/ResultViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.aplikasilaundry.ui.model
 
+import androidx.lifecycle.SavedStateHandle
+import com.example.aplikasilaundry.repositori.PesananRepository
+
+class ResultViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pesananRepository: PesananRepository
+)
+
 data class ItemDetailUiState(
     val outOfStock: Boolean = true,
     val detailLaundry: DetailLaundry = DetailLaundry()
