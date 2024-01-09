6d6af45c790ef3484cabb1cf9b78b03c734541a2
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-09 23:02:25 +0700

    Membuat FormPage dan menambahkan object FormDestinasi

---
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/FormPage.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasilaundry.ui.pages
+
+import com.example.aplikasilaundry.R
+import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+
+object FormDestinasi : DestinasiHalaman {
+    override val route = "item_entry"
+    override val judulHal = R.string.form
+}
