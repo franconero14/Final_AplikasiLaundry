05817688adbcbcaba91cef9c066b00174cea677f
franconero14 <nerobalor@gmail.com>
2024-01-09 21:29:17 +0700

    Add CenterAlignedTopAppBar in PengelolaHalaman.kt

--- app/src/main/java/com/example/aplikasilaundry/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/aplikasilaundry/navigasi/PengelolaHalaman.kt
@@ -1,6 +1,11 @@
 package com.example.aplikasilaundry.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,5 +19,17 @@ fun LaundryAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     modifier: Modifier = Modifier
 ) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (bisaNavigasiBack) {
+                IconButton(onClick = navigasiUp) {
+                    Icons.Default.ArrowBack
 
+                }
+            }
+        }
+    )
 }
