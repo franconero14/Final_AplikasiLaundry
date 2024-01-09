190d73e58568ea96557d8c580b43eaf1bd42ac47
franconero14 <nerobalor@gmail.com>
2024-01-09 15:32:58 +0700

    Add ViewModelFactory for OrderViewModel and FormViewModel

--- app/src/main/java/com/example/aplikasilaundry/ui/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/PenyediaViewModel.kt
@@ -1,10 +1,19 @@
 package com.example.aplikasilaundry.ui.model
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.aplikasilaundry.AplikasiLaundry
 
 object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { OrderViewModel(aplikasiLaundry().dataContainer.pesananRepository) }
+        initializer {
+            FormViewModel(aplikasiLaundry().dataContainer.pesananRepository)
+        }
+    }
 }
 
 fun CreationExtras.aplikasiLaundry(): AplikasiLaundry =
