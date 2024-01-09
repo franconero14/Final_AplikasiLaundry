b893887b3124b8d5456c79150375c0393f888dfd
franconero14 <nerobalor@gmail.com>
2024-01-09 21:31:56 +0700

    Add PenyediaNavigasi composable

--- app/src/main/java/com/example/aplikasilaundry/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/aplikasilaundry/navigasi/PengelolaHalaman.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,4 +33,12 @@ fun LaundryAppBar(
             }
         }
     )
+}
+
+@Composable
+fun PenyediaNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+
 }
