7fb2e09b1a1edb11dcaf7e1e77c3d0ed0ebd869e
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-09 22:49:34 +0700

    Menambahkan Column dan logo di HomePage

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/HomePage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/HomePage.kt
@@ -1,6 +1,7 @@
 package com.example.aplikasilaundry.ui.pages
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,11 +9,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.navigasi.DestinasiHalaman
 
@@ -39,7 +44,28 @@ fun Homepage(
                 .padding(50.dp)
                 .align(Alignment.CenterHorizontally)
         ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Image(painter = painterResource(id = R.drawable.logo), contentDescription = "")
+                Text(
+                    text = "Aplikasi",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 35.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
 
+                )
+                Text(
+                    text = "Laundry",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 55.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+            }
         }
     }
 }
