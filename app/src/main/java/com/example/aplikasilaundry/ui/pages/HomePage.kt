8ddff5eb7f5f9d8e912cadc24181f3933c0b5b05
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-09 22:10:54 +0700

    Membuat class HomePage dan menambahkan object HomeDestinasi

---
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/HomePage.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasilaundry.ui.pages
+
+import com.example.aplikasilaundry.R
+import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+
+object HomeDestinasi : DestinasiHalaman {
+    override val route = "laundry"
+    override val judulHal = R.string.app_name
+}
