8ce3e5ecc39b8c296d229c8455edb5e3d5116961
franconero14 <nerobalor@gmail.com>
2024-01-10 16:02:54 +0700

    Create dialog konfirmasi penghapusan data dengan dua pilihan tombol Iya dan Tidak

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -110,4 +112,20 @@ private fun DeleteDialog(
     onDeleteCancel: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        title = { Text(text = "Perhatian!") },
+        text = { Text(text = "Yakin ingin hapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Tidak")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Iya")
+            }
+        }
+    )
 }
