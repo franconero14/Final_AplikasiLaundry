9af8940b78a923b60be6e8bf7e5359107b966644
Prima06112002 <ramadhan06112002@gmail.com>
2024-01-11 00:52:02 +0700

    Menambahkan fun DetailScreen di ResultPage.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
@@ -7,33 +7,83 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.data.Pesanan
 import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+import com.example.aplikasilaundry.navigasi.LaundryAppBar
 import com.example.aplikasilaundry.ui.model.ItemDetailUiState
+import com.example.aplikasilaundry.ui.model.PenyediaViewModel
+import com.example.aplikasilaundry.ui.model.ResultViewModel
 import com.example.aplikasilaundry.ui.model.toLaundry
+import kotlinx.coroutines.launch
 
 object DataDestinasi : DestinasiHalaman {
     override val route = "item_details"
     override val judulHal = R.string.data_pesanan
     const val laundryIdArg = "itemId"
     val routeWithArgs = "$route/{$laundryIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: ResultViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            LaundryAppBar(
+                title = stringResource(DataDestinasi.judulHal),
+                bisaNavigasiBack = true,
+                navigasiUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        ItemDetailsBody(
+            itemDetailUiState = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteItem()
+                    navigateBack()
+                }
+            },
+            navigateToItemEdit = { navigateToEditItem(uiState.value.detailLaundry.id) },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(
+                    rememberScrollState()
+                ),
+
+            )
+    }
 }
 
 @Composable
