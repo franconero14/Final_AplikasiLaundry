8a7fefd523ccb36102dbd113139427eb945ccb20
franconero14 <nerobalor@gmail.com>
2024-01-10 15:43:45 +0700

    Add @Composable ItemDetailRow function

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
@@ -1,14 +1,20 @@
 package com.example.aplikasilaundry.ui.pages
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.data.Pesanan
@@ -54,5 +60,16 @@ private fun ItemDetails(
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
         }
+    }
+}
+
+@Composable
+private fun ItemDetailRow(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
 }
