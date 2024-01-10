aac6c94ca0872414df997681c73f30c72a2a07e7
franconero14 <nerobalor@gmail.com>
2024-01-10 16:51:23 +0700

    Add objek UpdateDestinasi in UpdatePage.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/UpdatePage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/UpdatePage.kt
@@ -1,2 +1,11 @@
 package com.example.aplikasilaundry.ui.pages
 
+import com.example.aplikasilaundry.R
+import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+
+object UpdateDestinasi : DestinasiHalaman {
+    override val route = "item_edit"
+    override val judulHal = R.string.edit
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
