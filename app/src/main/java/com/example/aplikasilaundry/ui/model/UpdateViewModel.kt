3f50ac2a7853a965a1d2d8710dfd752a06f6cb50
franconero14 <nerobalor@gmail.com>
2024-01-10 17:06:27 +0700

    Add ViewModel in UpdateViewModel.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
@@ -1,9 +1,32 @@
 package com.example.aplikasilaundry.ui.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.aplikasilaundry.repositori.PesananRepository
+import com.example.aplikasilaundry.ui.pages.UpdateDestinasi
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateViewModel(
     savedStateHandle: SavedStateHandle,
     private val pesananRepository: PesananRepository
-)
+) : ViewModel() {
+    var pesananUiState by mutableStateOf(UIStatePesanan())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[UpdateDestinasi.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            pesananUiState = pesananRepository.getPesananStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStatePesanan(true)
+        }
+    }
+}
