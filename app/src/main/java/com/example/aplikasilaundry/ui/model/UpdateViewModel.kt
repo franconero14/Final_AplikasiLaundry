a6e87b0da51ee31abaa8f4d32f92e430a4d8b7a9
franconero14 <nerobalor@gmail.com>
2024-01-10 16:45:09 +0700

    delete class UpdateViewModel.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aplikasilaundry.ui.model
-
-import androidx.lifecycle.SavedStateHandle
-import com.example.aplikasilaundry.repositori.PesananRepository
-
-class UpdateViewModel(
-    savedStateHandle: SavedStateHandle,
-    private val pesananRepository: PesananRepository
-)
