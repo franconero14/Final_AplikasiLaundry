ce4c880634b72a4eedd6eb0ed6bb7e8975e6a33c
franconero14 <nerobalor@gmail.com>
2024-01-10 16:46:38 +0700

    Create class UpdateViewModel.kt

---
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasilaundry.ui.model
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.aplikasilaundry.repositori.PesananRepository
+
+class UpdateViewModel {
+    class UpdateViewModel(
+        savedStateHandle: SavedStateHandle,
+        private val pesananRepository: PesananRepository
+    )
+}
