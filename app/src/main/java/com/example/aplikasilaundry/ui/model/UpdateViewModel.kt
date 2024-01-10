db69d24ddc3bf3ed64a02f802fd2e10564b75ba6
franconero14 <nerobalor@gmail.com>
2024-01-10 16:47:39 +0700

    Revisi class UpdateViewModel.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/model/UpdateViewModel.kt
@@ -3,9 +3,7 @@ package com.example.aplikasilaundry.ui.model
 import androidx.lifecycle.SavedStateHandle
 import com.example.aplikasilaundry.repositori.PesananRepository
 
-class UpdateViewModel {
-    class UpdateViewModel(
-        savedStateHandle: SavedStateHandle,
-        private val pesananRepository: PesananRepository
-    )
-}
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pesananRepository: PesananRepository
+)
