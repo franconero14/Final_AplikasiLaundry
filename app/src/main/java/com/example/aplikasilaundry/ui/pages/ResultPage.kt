5422cb7cdb46dfdaf3c6e60714b2ff5186df8b76
franconero14 <nerobalor@gmail.com>
2024-01-10 15:27:08 +0700

    add ItemDetailsBody in ResultPage.kt

--- app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
+++ app/src/main/java/com/example/aplikasilaundry/ui/pages/ResultPage.kt
@@ -1,11 +1,24 @@
 package com.example.aplikasilaundry.ui.pages
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.aplikasilaundry.R
 import com.example.aplikasilaundry.navigasi.DestinasiHalaman
+import com.example.aplikasilaundry.ui.model.ItemDetailUiState
 
 object DataDestinasi : DestinasiHalaman {
     override val route = "item_details"
     override val judulHal = R.string.data_pesanan
     const val laundryIdArg = "itemId"
     val routeWithArgs = "$route/{$laundryIdArg}"
+}
+
+@Composable
+private fun ItemDetailsBody(
+    itemDetailUiState: ItemDetailUiState,
+    onDelete: () -> Unit,
+    navigateToItemEdit: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
 }
