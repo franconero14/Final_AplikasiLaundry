a3b3a8ffd7221c4bdda5471a4dc52706be15ce75
franconero14 <nerobalor@gmail.com>
2024-01-11 12:20:23 +0700

    update AndroidManifest.xml and MainActivity.kt

--- app/src/main/java/com/example/aplikasilaundry/MainActivity.kt
+++ app/src/main/java/com/example/aplikasilaundry/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplikasilaundry.navigasi.LaundryApp
 import com.example.aplikasilaundry.ui.theme.AplikasiLaundryTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LaundryApp()
                 }
             }
         }
